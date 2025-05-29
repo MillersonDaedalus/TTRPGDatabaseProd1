d83105fd1637071bbf2ac6dd07f39e94a7e6f5a6
MillersonDaedalus <tom.millerson@proton.me>
2025-05-29 11:26:36 -0600

    Removed old temp files that have been integrated into the new django framework

--- Testing.py
+++
@@ -1,8 +0,0 @@
-import CharacterCreation as CC
-
-if __name__ == "__main__":
-    races = ['Elf','Orc']
-    for i in range(2):
-        globals()['character'+str(i)] = CC.Character(races[i])
-    print(character0.race)
-    print(character1.race)
